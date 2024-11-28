4cafa1757d06d7adbf3032b1c360e5c1f6ed6043
tspu_02 <tspu_02@mail.ru>
2024-11-28 15:47:35 +0700

    web

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField(
+        max_length=255,
+        )
+
+    text = models.CharField(
+        max_length=1000,
+        )
+
