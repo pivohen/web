359368c153e5ea4713634a8f235039c0c35f0284
tspu_02 <tspu_02@mail.ru>
2024-12-12 15:51:15 +0700

    add category. and added view

--- blog/models.py
+++ blog/models.py
@@ -10,3 +10,24 @@ class Article(models.Model):
         max_length=1000,
         )
 
+    decription = models.TextField(
+        null=True,
+        blank=True,
+    )
+    category = models.ForeignKey(
+        'Category', 
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+    def __str__(self):
+        return "Статья: " + '"' + self.title + '"'
+    # Reporter, on_delete=models.CASCADE 
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=255,
+    )
+
+    def __str__(self):
+        return self.name
